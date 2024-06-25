e3ff1698b98543fe461d76930c57603e51df9cbf
Enamulx <coderxdante@gmail.com>
2024-06-25 17:56:36 +0600

    lyricz

--- components/Home/our-products.vue
+++ components/Home/our-products.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="bg-[#f9f6f2]">
+	<section class="bg-[#f9f6f2] pb-36">
 		<UContainer>
 			<CommonSectionTitle
 				alignment="text-center"
@@ -7,20 +7,32 @@
 				title="See Best of Our Developer"
 			/>
 
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-5 mb-10">
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-5 mx-10">
 				<div class="relative">
 					<img
 						class="rounded-2xl"
 						src="/public/our-products/project-banner3.jpg"
 						alt=""
 					/>
-					<div class="absolute left-10 -bottom-5 bg-white m-10">
-						<h1>OXYMANAGER</h1>
-						<p>
-							OxyManager is a Business all in one Management Tool A complete
-							business management tool - which will make your life easier
-							www.oxymanager.com
-						</p>
+					<div
+						class="absolute left-10 right-10 -bottom-28 bg-white m-5 p-5 rounded-xl"
+					>
+						<div class="mb-10">
+							<h1>OXYMANAGER</h1>
+							<br />
+							<p>
+								<a class="font-bold" href=""
+									>OxyManager is a Business all in one...</a
+								>
+								<br />
+								<a href=""
+									>A complete business Management Tool A complete business
+									management tool - which will make your life easier</a
+								>
+							</p>
+							<br />
+							<a href="www.oxymanager.com">www.oxymanager.com</a>
+						</div>
 					</div>
 				</div>
 				<div>
--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,14 @@ definePageMeta({
 </script>
 
 <template>
-<<<<<<< HEAD
 	<!-- <HomeBanner /> -->
 	<HomeAboutUs />
 	<HomeOurProducts />
 	<HomeOurPartners />
-=======
-  <HomeBanner />
-  <HomeOurDevelopedProducts />
-  <HomeStats />
-  <HomeGetInTouch />
-  <HomeClientsReview />
-  <HomeOurPartners />
->>>>>>> fab1cc862be606f1caac51210089a5cdd893dc0b
+	<HomeBanner />
+	<HomeOurDevelopedProducts />
+	<HomeStats />
+	<HomeGetInTouch />
+	<HomeClientsReview />
+	<HomeOurPartners />
 </template>
