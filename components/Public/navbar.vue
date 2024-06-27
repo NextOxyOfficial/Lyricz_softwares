3397d73eeaaca3496b739eafdb2f9ffbf54e02da
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-06-27 10:58:10 +0600

    Navbar Updated

--- components/Public/navbar.vue
+++ components/Public/navbar.vue
@@ -1,8 +1,6 @@
 <script setup lang="">
-const open = ref(false);
+const isOpen = ref(false);
 const scrollPosition = ref(0);
-
-console.log(open.value);
 
 // Function to log scroll position
 const logScrollPosition = () => {
@@ -22,7 +20,7 @@ onMounted(() => {
 <template>
   <section
     :class="scrollPosition > 250 ? 'sticky top-0 z-10' : ''"
-    class="bg-gray-100 md:py-4 transition-all duration-1000 ease-in-out"
+    class="bg-gray-100 py-2 md:py-4 transition-all duration-1000 ease-in-out"
   >
     <UContainer>
       <div class="flex items-center w-full justify-between">
@@ -42,18 +40,71 @@ onMounted(() => {
             >Contact</NuxtLink
           >
         </ul>
+
         <div
-          @click="open = !open"
+          @click="isOpen = true"
           class="md:hidden flex flex-col justify-between gap-[6px] mr-2 transition-all duration-1000 ease-in-out"
         >
-          <div class="bg-gray-800 h-1 w-10"></div>
-          <div class="bg-gray-800 h-1 w-10" :class="open ? 'hidden' : ''"></div>
-          <div
-            class="bg-gray-800 h-1 w-10"
-            :class="open ? 'rotate-45' : ''"
-          ></div>
+          <UIcon name="ic:baseline-menu" dynamic class="text-4xl" />
         </div>
       </div>
+
+      <!-- Slide Over -->
+      <USlideover v-model="isOpen" prevent-close>
+        <UCard
+          class="flex flex-col flex-1"
+          :ui="{
+            body: { base: 'flex-1' },
+            ring: '',
+            divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+          }"
+        >
+          <template #header>
+            <div class="flex items-center justify-between">
+              <h3
+                class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+              >
+                <NuxtLink to="/">
+                  <NuxtImg
+                    src="/logo/white-logo.png"
+                    alt="Logo"
+                    class="h-12 md:h-16"
+                  />
+                </NuxtLink>
+              </h3>
+              <UButton
+                color="gray"
+                variant="ghost"
+                size=""
+                icon="i-heroicons-x-mark-20-solid"
+                class="-my-1 text-5xl"
+                @click="isOpen = false"
+              />
+            </div>
+          </template>
+
+          <Placeholder class="h-full" />
+          <ul
+            class="flex gap-6 flex-col items-center text-xl text-gray-700 font-medium"
+          >
+            <NuxtLink class="hover:text-blue-400 font-bold" to="/"
+              >Home</NuxtLink
+            >
+            <NuxtLink class="hover:text-blue-400 font-bold" to="/"
+              >About Us</NuxtLink
+            >
+            <NuxtLink class="hover:text-blue-400 font-bold" to="/"
+              >Our Services</NuxtLink
+            >
+            <NuxtLink class="hover:text-blue-400 font-bold" to="/"
+              >Faqs</NuxtLink
+            >
+            <NuxtLink class="hover:text-blue-400 font-bold" to="/"
+              >Contact</NuxtLink
+            >
+          </ul>
+        </UCard>
+      </USlideover>
     </UContainer>
   </section>
 </template>
