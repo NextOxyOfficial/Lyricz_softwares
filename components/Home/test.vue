b0010d04133e2c3999cba3158ad864537ee33a8f
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-06-28 22:13:04 +0600

    updated

--- components/Home/test.vue
+++ components/Home/test.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="relative container mx-auto h-96">
     <CommonParallaxScene class="absolute">
-      <div data-depth="0.2">
-        <img src="/logo/dark-logo.png" alt="Dark Logo" />
-      </div>
+      <div data-depth="0.2">sdfsdf</div>
       <div data-depth="0.1">My fourth Layer!</div>
       <div data-depth="0.2">My fifth Layer!</div>
     </CommonParallaxScene>
